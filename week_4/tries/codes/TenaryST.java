676fd287d6dc9acfb6860f2d25f4cc5e14e7443c
ThanhPhuoc <thanhphuocb7@gmail.com>
2024-01-11 12:50:33 +0700

    added TST

---
+++ week_4/tries/codes/TenaryST.java
@@ -0,0 +1,55 @@
+/**
+ * TernaryST Ternary Search Tree
+ */
+public class TernaryST<Value> {
+    private Node root = new Node();
+
+    private class Node {
+        private char c;
+        private Node left, mid, right;
+        private Value val;
+    }
+
+    public void put(String key, Value val) {
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key, Value val, int d) {
+        c = key.charAt(d);
+        if (x == null) {
+            x = new Node();
+            x.c = c;
+        }
+        if (c < x.c)
+            x.left = put(x.left, key, val, d);
+        else if (c > x.c) {
+            x.right = put(x.right, key, val, d);
+        } else if (d < key.length()) {
+            x.mid = put(x.mid, key, val, d + 1);
+        } else {
+            x.val = val;
+        }
+        return x;
+    }
+
+    public Value get(String key) {
+        Node x = get(root, key, 0);
+        if (x == null)
+            return null;
+        return x.val;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null)
+            return null;
+        char c = key.charAt(d);
+        if (c < x.c)
+            return get(x.left, key, d);
+        else if (c > x.c)
+            return get(x.right, key, d);
+        else if (d < key.length() - 1)
+            return get(x.mid, key, d + 1);
+        else
+            return x;
+    }
+}
